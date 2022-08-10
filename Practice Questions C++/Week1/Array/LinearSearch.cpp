49209097ddc7faa82bc46d71e51b86ae09ec68be
Majid Majeed <108627051+Jehlum01@users.noreply.github.com>
2022-08-10 23:23:21 +0530

    Linear Search
    To  find the given number in the array is present or not

---
+++ Practice Questions C++/Week1/Array/LinearSearch.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                             linearSearch in array
+*******************************************************************************/
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int
+linearSearch (int arry[], int n, int key)
+{
+  for (int i = 0; i < n; i++)
+    {
+      if (arry[i] == key)
+	{
+	  return i;
+	}
+    }
+  return -1;
+}
+
+int
+main ()
+{
+  int n;
+  cin >> n;
+  int arry[n];
+  for (int i = 0; i < n; i++)
+    {
+      cin >> arry[i];
+    }
+
+  int key;
+  cin >> key;
+
+  cout << linearSearch (arry, n, key) << endl;
+
+
+  return 0;
+}
